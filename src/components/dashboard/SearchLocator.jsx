7af75e5703d75c188c1cdc28dddca71176fccd3e
Michael <themichaelsteve@gmail.com>
2024-10-09 11:54:57 +0100

    SearchBar logic and responsiveness. UI fix

--- src/components/dashboard/SearchLocator.jsx
+++ src/components/dashboard/SearchLocator.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import SearchBar from '../global/SearchBar'
 import Header from '../global/Header'
-import Button from '../global/Button'
 import UseCurrentLocation from '../global/UseCurrentLocation'
 
 const SearchLocator = () => {
+  const weatherImage = useRef(null);
+  const [isFocused, setIsFocused] = useState(null);
+
+  const handleWeatherImageFocus = () => {
+    setIsFocused(true);
+  }
+
+  const handleWeatherImageBlur = () => {
+    setIsFocused(false);
+  }
+
+
   return (
     <>
       <div className='absolute top-0 xl:hidden'>
         <Header backBtn={false} logo={true} shareBtn={false} />
       </div>
-        <div className='flex flex-col justify-center gap-7 xl:justify-between items-center py-6 h-full xl:w-[273px] '>
-          <div className='xl:hidden w-[70%] lg:w-[80%]'><img className='w-full' src='/weather-location-enabled.svg'/></div>
-          <SearchBar />
+        <div className='flex flex-col justify-center gap-7 xl:justify-between items-center py-6 h-full w-[273px] xl:w-[273px] '>
+          <div ref = {weatherImage} className= {`xl:hidden transition-all ease-in duration-300 ${ isFocused ? 'max-w-[30%] lg:max-w-[40%]' : 'max-w-[70%] lg:max-w-[80%]'}`}><img className='w-full' src='/weather-location-enabled.svg'/></div>
+          <SearchBar onFocus = {handleWeatherImageFocus} onBlur = {handleWeatherImageBlur} />
           <div className='xl:hidden w-[65%]' ><img src='/divider.svg'/></div>
           <UseCurrentLocation />
         </div>
--- src/components/global/SearchBar.jsx
+++ src/components/global/SearchBar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { useWeather } from '../../Context/WeatherContext'
 import fetchWeather from '../../API/FetchWeather'
 
-const SearchBar = () => {
+const SearchBar = ({onFocus, onBlur}) => {
  const [list, setList] = useState([])
  const [city, setCity] = useState('')
  const [error, setError] = useState(null)
@@ -51,14 +51,16 @@ const SearchBar = () => {
     <div className='flex flex-col gap-0 justify-center items-center w-full'>
         <form ref={cityForm} onSubmit = {handleSubmit} className='bg-white flex flex-row gap-2 items-center justify-start rounded-3xl py-2 pl-4 w-full'>
         <img src='/search.svg' />
-        <input className='placeholder-primary text-primary-dark outline-none text-sm w-[90%]' type='text' 
+        <input className='placeholder-primary text-primary-dark focus:outline-none text-sm w-full mr-1  bg-transparent border-none appearance-none' type='text' 
         name='search' 
         placeholder='Search for a city or local'
         onChange={handleChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
         />
         </form>
         {list.length > 0 && (
-          <div className='flex flex-col gap-0 w-full mt-2'>
+          <div className='flex flex-col gap-0 w-[273px] mt-2 h-[150px] overflow-y-scroll xl:h-auto'>
             {list.map((place,index) => (
               <div onClick={() => handleFetchWeather(place.lat, place.lon)} key={index} className='block w-full text-white py-2 border-[#1e1e1e] border-b-[1px] cursor-pointer'>
                 {place.name}, {place.state}, {place.country}
