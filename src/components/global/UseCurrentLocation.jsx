77c15ef552bf99a4701fdf24367005c2d8481c2d
Michael <themichaelsteve@gmail.com>
2024-10-12 13:38:57 +0100

    Handled GeoError

--- src/components/global/UseCurrentLocation.jsx
+++ src/components/global/UseCurrentLocation.jsx
@@ -1,7 +1,8 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState , useEffect, useRef} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useWeather } from '../../Context/WeatherContext'
 import Button from './Button'
+import SearchBar from './SearchBar'
 
 const UseCurrentLocation = () => {
     const { setWeatherData } = useWeather();
@@ -9,18 +10,29 @@ const UseCurrentLocation = () => {
     const [lat, setLat] = useState();
     const [lon, setLon] = useState();
     const [error, setError] = useState(null)
+    const [geoError, setGeoError] = useState(null);
+    const [loading, setLoading] = useState(null);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-            setLat(position.coords.latitude);
-            setLon(position.coords.longitude);
-        },
-        (error) => {
-            setError(error)
-        }
-    );
-},[])
+    const findLocation = () => {
+      setLoading('Getting your location...')
+      setGeoError(null);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setLat(position.coords.latitude);
+        setLon(position.coords.longitude);
+        setGeoError(null);
+        setLoading('Fetching weather details...')
+        fetchWeather(latitude,longitude);
+    },
+    (geoError) => {
+      setLoading(null);
+        setGeoError('Location access denied. To use location please enable it in your browser settings and reload the page.')
+    }
+  );
+};     
+
 
     const fetchWeather = (lat, lon) => {
           const key = import.meta.env.VITE_RAIN_API_KEY;
@@ -38,20 +50,55 @@ const UseCurrentLocation = () => {
             })
             .catch((error) => setError(error.message));
           } else {
-            setError('Getting your location...')
+            setError('Location not found')
           }
           
        };
 
+       const handleGeoError = () => {
+        window.location.reload();
+      };
+
+      const handleInputFocus = () => {
+        setGeoError(null);
+      }
 
   return (
     <>
+      {loading && 
+        <div className='text-secondary text-xs flex flex-col gap-1 justify-center items-center mb-4'>
+        {loading}
+        </div>
+      }
       {error && <div className='text-accent text-xs flex flex-col gap-1 justify-center items-center mb-4'>
           {error}
           <button onClick={() => setError(null)} className='underline'>Try again</button>
           </div>
           }
-      <Button onClick={() => fetchWeather(lat, lon)} className={'text-primary-dark bg-secondary'} icon = {<img src='/locator.svg'/>} text ='Use current location' />
+
+      {geoError && 
+        <div className='text-accent text-xs text-center flex flex-col gap-1 justify-center items-center mb-4'>
+          {geoError}
+          <div className='flex flex-col gap-4 justify-center items-center mt-4'>
+            <Button 
+            onClick={handleGeoError} 
+            className= {'bg-primary text-white'}
+            text= 'Reload page'
+            />
+            <Button 
+            onClick={handleInputFocus} 
+            className= {'bg-transparent text-secondary border border-solid border-secondary'}
+            text= 'Search manually'
+            />
+          </div>
+        </div>
+      }
+      <Button 
+      onClick={findLocation} 
+      className={'text-primary-dark bg-secondary'} 
+      icon = {<img src='/locator.svg'/>} 
+      text ='Use current location' 
+      />
       
     </>
 
